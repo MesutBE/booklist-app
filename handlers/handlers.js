098124bd61c2a8ddc423b445c04971fe2ea90637
mesut <gelukkig.zijn.be@gmail.com>
2020-04-03 08:49:26 +0200

    Add Book and Display
    * Handlers: Create handlers in a class to add books (now from hard coded values) and display them.
    * Interactions: add event listener to display added books.

---
+++ handlers/handlers.js
@@ -0,0 +1,38 @@
+
+// UI Class: Handle UI Tasks
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '234567'
+            },
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '234567'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    // Add books
+    static addBookToList(book) {
+        const list = document.querySelector('#book-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+      <td>${book.title}</td>
+      <td>${book.author}</td>
+      <td>${book.isbn}</td>
+      <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+    `;
+
+        list.appendChild(row);
+    }
+}
